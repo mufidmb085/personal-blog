e057ba91b845f6091d9e545b1d1082b1ffd2bab3
mufidmb085 <mufidmb085@gmail.com>
2025-07-02 21:21:41 +0700

    feat(navbar): remove site name title

--- components/ui/NavigationBar.tsx
+++ components/ui/NavigationBar.tsx
@@ -14,7 +14,6 @@ const NavigationBar = () => {
               <NavbarBrand>
                   <Link href="/">
                       <Logo />
-                      <p>{siteConfig.name}</p>
                   </Link>
               </NavbarBrand>
           </NavbarContent>
