a5e40482d76b7c008c4908ce98005147700d3d6a
mufidmb085 <mufidmb085@gmail.com>
2025-07-02 21:29:00 +0700

    feat(navbar): move navbar item to the start side next to the logo

--- components/ui/NavigationBar.tsx
+++ components/ui/NavigationBar.tsx
@@ -10,28 +10,30 @@ import {Link} from "@heroui/link";
 const NavigationBar = () => {
   return (
       <Navbar maxWidth={"xl"}>
-          <NavbarContent>
-              <NavbarBrand>
-                  <Link href="/">
-                      <Logo />
-                  </Link>
-              </NavbarBrand>
+          <NavbarContent justify={"start"}>
+              <div className="flex flex-row items-center justify-start">
+                  <NavbarBrand>
+                      <Link href="/">
+                          <Logo />
+                      </Link>
+                  </NavbarBrand>
+                  <div className={"flex flex-row justify-center items-center gap-4"}>
+                      <div className={"flex flex-row"}>
+                          <ul className="hidden lg:flex gap-4 justify-start ml-2">
+                              {siteConfig.navItems.map((item) => (
+                                  <NavbarItem key={item.href}>
+                                      <NextLink className={clsx(link({ color: "foreground" }), "data-[active=true]:text-primary data-[active=true]:font-medium",)} color="foreground" href={item.href}>
+                                          {item.label}
+                                      </NextLink>
+                                  </NavbarItem>
+                              ))}
+                          </ul>
+                      </div>
+                  </div>
+              </div>
           </NavbarContent>
           <NavbarContent justify={"end"}>
-              <div className={"flex flex-row justify-center items-center gap-4"}>
-                  <div className={"flex flex-row"}>
-                      <ul className="hidden lg:flex gap-4 justify-start ml-2">
-                          {siteConfig.navItems.map((item) => (
-                              <NavbarItem key={item.href}>
-                                  <NextLink className={clsx(link({ color: "foreground" }), "data-[active=true]:text-primary data-[active=true]:font-medium",)} color="foreground" href={item.href}>
-                                      {item.label}
-                                  </NextLink>
-                              </NavbarItem>
-                          ))}
-                      </ul>
-                  </div>
                   <ThemeSwitch />
-              </div>
           </NavbarContent>
       </Navbar>
   )
