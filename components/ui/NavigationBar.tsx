29c184e83e0bc4f71be7789876ec1d1e04a54a9f
mufidmb085 <mufidmb085@gmail.com>
2025-07-02 22:32:41 +0700

    feat(navbar): use button for navbar items and highlight active route

--- components/ui/NavigationBar.tsx
+++ components/ui/NavigationBar.tsx
@@ -1,42 +1,44 @@
+"use client";
+
 import {Navbar, NavbarBrand, NavbarContent, NavbarItem} from "@heroui/navbar";
 import {siteConfig} from "@/config/site";
 import {Logo} from "@/components/ui/Icons";
-import NextLink from "next/link";
-import clsx from "clsx";
 import {ThemeSwitch} from "@/components/ui/ThemeSwitch";
-import {link} from "@heroui/theme";
 import {Link} from "@heroui/link";
+import {Button} from "@heroui/button";
+import {usePathname} from "next/navigation";
 
 const NavigationBar = () => {
-  return (
-      <Navbar maxWidth={"xl"}>
-          <NavbarContent justify={"start"}>
-              <div className="flex flex-row items-center justify-start">
-                  <NavbarBrand>
-                      <Link href="/">
-                          <Logo />
-                      </Link>
-                  </NavbarBrand>
-                  <div className={"flex flex-row justify-center items-center gap-4"}>
-                      <div className={"flex flex-row"}>
-                          <ul className="hidden lg:flex gap-4 justify-start ml-2">
-                              {siteConfig.navItems.map((item) => (
-                                  <NavbarItem key={item.href}>
-                                      <NextLink className={clsx(link({ color: "foreground" }), "data-[active=true]:text-primary data-[active=true]:font-medium",)} color="foreground" href={item.href}>
-                                          {item.label}
-                                      </NextLink>
-                                  </NavbarItem>
-                              ))}
-                          </ul>
-                      </div>
-                  </div>
-              </div>
-          </NavbarContent>
-          <NavbarContent justify={"end"}>
-                  <ThemeSwitch />
-          </NavbarContent>
-      </Navbar>
-  )
+
+    const pathname = usePathname();
+
+    return (
+        <Navbar maxWidth={"xl"}>
+            <NavbarContent justify={"start"}>
+                <div className="flex flex-row items-center justify-start gap-4">
+                    <NavbarBrand>
+                        <Link href="/">
+                            <Logo />
+                        </Link>
+                     </NavbarBrand>
+                     <div className={"flex flex-row justify-center items-center"}>
+                        <div className={"flex flex-row"}>
+                              <ul className="hidden lg:flex gap-2 justify-start">
+                                  {siteConfig.navItems.map((item) => (
+                                   <NavbarItem key={item.href}>
+                                         <Button as={Link} variant={pathname === item.href || pathname.startsWith(`${item.href}/`) ? "flat" : "light"} color={"primary"} href={item.href} className={"text-md"}>{item.label}</Button>
+                                     </NavbarItem>
+                                 ))}
+                            </ul>
+                        </div>
+                    </div>
+                 </div>
+             </NavbarContent>
+             <NavbarContent justify={"end"}>
+                    <ThemeSwitch />
+            </NavbarContent>
+        </Navbar>
+    )
 }
 
 export default NavigationBar;
