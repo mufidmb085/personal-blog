dcfa174cba31aaefa80e55a06d1d005b3fa10391
mufidmb085 <mufidmb085@gmail.com>
2025-07-05 16:33:31 +0700

    feat(navbar): adjust navbar and navbar menu
    - Enable navbar hide on scroll
    - Remove unused div
    - Replace button to link
    - Simplify active route logic
    - adjust responsiveness

--- components/ui/NavigationBar.tsx
+++ components/ui/NavigationBar.tsx
@@ -13,7 +13,6 @@ import {siteConfig} from "@/config/site";
 import {Logo} from "@/components/ui/Icons";
 import {ThemeSwitch} from "@/components/ui/ThemeSwitch";
 import {Link} from "@heroui/link";
-import {Button} from "@heroui/button";
 import {usePathname} from "next/navigation";
 import React from "react";
 
@@ -22,39 +21,39 @@ const NavigationBar = () => {
     const pathname = usePathname();
     const [isMenuOpen, setIsMenuOpen] = React.useState(false);
 
+    function isPathnameSame(href: string): boolean {
+        return pathname === href || pathname.startsWith(`${href}/`)
+    }
+
     return (
-        <Navbar maxWidth={"xl"} isMenuOpen={isMenuOpen} onMenuOpenChange={setIsMenuOpen}>
+        <Navbar maxWidth={"xl"} isMenuOpen={isMenuOpen} shouldHideOnScroll={true} onMenuOpenChange={setIsMenuOpen}>
             <NavbarContent justify={"start"}>
                 <div className="flex flex-row items-center justify-start gap-4">
                     <NavbarBrand>
                         <Link href="/">
-                            <Logo />
+                            <Logo size={48} />
                         </Link>
                      </NavbarBrand>
-                     <div className={"flex flex-row justify-center items-center"}>
-                        <div className={"flex flex-row"}>
-                              <ul className="hidden md:flex gap-2 justify-start">
-                                  {siteConfig.navItems.map((item) => (
-                                   <NavbarItem key={item.href}>
-                                         <Button as={Link} variant={pathname === item.href || pathname.startsWith(`${item.href}/`) ? "flat" : "light"} color={"primary"} href={item.href} className={"text-md"}>{item.label}</Button>
-                                     </NavbarItem>
-                                 ))}
-                            </ul>
-                        </div>
-                    </div>
+                    <ul className="hidden lg:flex gap-4 justify-start">
+                        {siteConfig.navItems.map((item) => (
+                            <NavbarItem key={item.href}>
+                                <Link color={isPathnameSame(item.href) ? "primary" : "foreground"} href={item.href}>{item.label}</Link>
+                            </NavbarItem>
+                        ))}
+                    </ul>
                  </div>
              </NavbarContent>
              <NavbarContent justify={"end"}>
                  <div className="flex flex-row items-center justify-start gap-4">
                      <ThemeSwitch />
-                     <NavbarMenuToggle aria-label={isMenuOpen ? "Close menu" : "Open menu"} className="sm:hidden" />
+                     <NavbarMenuToggle aria-label={isMenuOpen ? "Close menu" : "Open menu"} className="lg:hidden" />
                  </div>
             </NavbarContent>
 
             <NavbarMenu>
                 {siteConfig.navMenuItems.map((item, index) => (
                     <NavbarMenuItem isActive={pathname === item.href || pathname.startsWith(`${item.href}/`)} key={`${item}-${index}`}>
-                        <Button as={Link} variant={pathname === item.href || pathname.startsWith(`${item.href}/`) ? "flat" : "light"} color={"primary"} size={"lg"} href={item.href} fullWidth={true} className={"text-md flex-row justify-start p-4 m-0"} onPress={() => setIsMenuOpen(false)}>{item.label}</Button>
+                        <Link color={isPathnameSame(item.href) ? "primary" : "foreground"} href={item.href} onPress={() => {setIsMenuOpen(false)}}>{item.label}</Link>
                     </NavbarMenuItem>
                 ))}
             </NavbarMenu>
