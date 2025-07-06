4b8d7c807189b2c79507b17162a5385a950da7d5
mufidmb085 <mufidmb085@gmail.com>
2025-07-07 03:25:35 +0700

    revert(home-page): undo mistaken changes from 8be1936

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,6 @@ export default async function Home() {
         <div className={"flex flex-col justify-center lg:grid lg:grid-cols-2 lg:grid-rows-3 gap-8 w-full max-w-screen-xl p-8"}>
             <HeroSection className={"lg:col-span-1 self-start"} />
             <LatestArticlesSection className={"lg:col-span-1 lg:row-span-3 self-center"} />
-            <p className={"lg:col-span-1"}>About Me</p>
         </div>
     );
 }
