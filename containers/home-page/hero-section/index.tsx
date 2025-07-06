901cbc6ca56ab5ef8d289b825d6464148154a3ac
mufidmb085 <mufidmb085@gmail.com>
2025-07-07 03:26:39 +0700

    feat(home-page): remove shadow in main headline and adjust text tracking and leading to tight in subheadline

--- containers/home-page/hero-section/index.tsx
+++ containers/home-page/hero-section/index.tsx
@@ -10,8 +10,8 @@ const HeroSection = ({className}: HeroSectionProps) => {
 	return (
 		<section className={clsx(className)}>
 			<div className={"flex flex-col gap-4 max-w-lg items-start justify-center"}>
-				<h1 className={clsx("text-6xl text-primary font-bold uppercase tracking-tight drop-shadow-md", fontPlayfairDisplaySC.className)}>From Thought<br/>to Screen</h1>
-				<h2 className={"text-lg text-foreground font-light"}>I document my thoughts, ideas, and things I build.<br />Mostly as a way to think clearer, but you&#39;re welcome to read along.</h2>
+				<h1 className={clsx("text-6xl text-primary font-bold uppercase tracking-tight", fontPlayfairDisplaySC.className)}>From Thought<br/>to Screen</h1>
+				<h2 className={"text-lg text-foreground tracking-tight leading-tight"}>I document my thoughts, ideas, and things I build.<br />Mostly as a way to think clearer, but you&#39;re welcome to read along.</h2>
 			</div>
 		</section>
 	)
