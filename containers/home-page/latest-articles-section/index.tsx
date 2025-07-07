194a1976dbcb6f5fe8b031b89f31c0f488fc99c5
mufidmb085 <mufidmb085@gmail.com>
2025-07-08 03:07:54 +0700

    feat(home-page) change latest section from using div to using card component

--- containers/home-page/latest-articles-section/index.tsx
+++ containers/home-page/latest-articles-section/index.tsx
@@ -3,6 +3,7 @@ import {getAllArticleMetadata} from "@/libs/articles";
 import ArticleCard from "@/components/blog/articles/ArticleCard";
 import {Link} from "@heroui/link";
 import clsx from "clsx";
+import {Card, CardBody, CardFooter, CardHeader} from "@heroui/card";
 
 interface LatestArticlesSectionProps {
 	className?: string;
@@ -14,12 +15,12 @@ const LatestArticlesSection = ({className}: LatestArticlesSectionProps) => {
 
 	return (
 		<section className={clsx(className)}>
-			<div className={"flex flex-col max-w-md gap-6 bg-content1 rounded-2xl p-8 place-content-center"}>
-				<div className={"flex flex-col items-center justify-center gap-2"}>
-					<h1 className={"text-2xl text-primary font-bold uppercase text-center"}>Latest Articles</h1>
-					<h2 className={"text-lg text-content1-foreground font-light items-center text-center"}>Things I&#39;ve been thinking and writing about lately</h2>
-				</div>
-				<div className={"flex flex-col gap-4"}>
+			<Card disableRipple={true} disableAnimation={true} shadow={"sm"} allowTextSelectionOnPress={false} className={"max-w-lg gap-6 bg-content1 p-8"}>
+				<CardHeader className={"flex flex-col items-center justify-center gap-2"}>
+					<h1 className={"text-4xl text-primary font-semibold uppercase text-center"}>Latest Articles</h1>
+					<h2 className={"text-lg text-content1-foreground items-center text-center"}>Things I&#39;ve been thinking and writing about lately</h2>
+				</CardHeader>
+				<CardBody className={"flex flex-col gap-4 items-center justify-center"}>
 					{articlesMetadata.slice(0, 3).map((metadata) => (
 						<ArticleCard
 							key={metadata.slug}
@@ -31,11 +32,13 @@ const LatestArticlesSection = ({className}: LatestArticlesSectionProps) => {
 							slug={metadata.slug}
 						/>
 					))}
-				</div>
-				{articlesMetadata.length > 3 && (
-					<Link href={"/blog"} className={"items-center justify-center"}>Discover More</Link>
-				)}
-			</div>
+				</CardBody>
+				<CardFooter className={"flex flex-col gap-4 items-center justify-center"}>
+					{articlesMetadata.length > 3 && (
+						<Link href={"/blog"} className={"items-center justify-center"}>Discover More</Link>
+					)}
+				</CardFooter>
+			</Card>
 		</section>
 	)
 }
